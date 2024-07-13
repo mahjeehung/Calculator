b5b101d3b55701a26e1a3c9ce27851398a2522a2
mahjeehung <151704278+mahjeehung@users.noreply.github.com>
2024-07-13 20:52:51 +0800

    Setup, math functions
    Set up files and basic math functions

---
+++ js.js
@@ -0,0 +1,20 @@
+function add(x,y){
+    return x+y
+}
+
+function subtract(x,y){
+    return x-y
+}
+
+function multiply(x,y){
+    return x*y
+}
+
+function divide(x,y){
+    return x / y
+}
+
+console.log(add(6,3))
+console.log(subtract(6,3))
+console.log(multiply(6,3))
+console.log(divide(6,3))
